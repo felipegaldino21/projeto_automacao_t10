1def4cd4a03a18dc7c9821848673881b9b0a97ad
FelipeGaldino <felipegaldino21@gmail.com>
2023-07-11 19:58:07 -0300

    Commit inicial do projeto

---
+++ features/spec/login.feature
@@ -0,0 +1,10 @@
+#language: pt
+
+Funcionalidade: Login
+
+Cenario: Fazer login com sucesso
+
+Dado que eu esteja na tela de "login"
+Quando preencho o formulario de "Login de acesso"
+E clico no botao "Entrar"
+Entao devo ser redirecionado para a tela "home"
---
+++ features/step_definitions/login.rb
@@ -0,0 +1,11 @@
+Quando('preencho o formulario de {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('clico no botao {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Entao('devo ser redirecionado para a tela {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
